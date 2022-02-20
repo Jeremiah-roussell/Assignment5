183feeb32482cf5c0bfe86e62e0f573e07199f3b
Jeremiah Roussell <jjroussell1@buffs.wtamu.edu>
2022-02-20 13:42:19 -0600

    Ready To Grade!

---
+++ UpdatedTest/Assignment3.Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Assignment3.Tests;
+using Assignment3.Models;
+
+namespace Assignment3
+{
+    public class DataAccessLayerTest
+    {
+        [Fact]
+        public async Task GetCustomersAsync_CustomersAreReturned()
+        {
+            using (var db = new DeliveryCartDbContext(Utilities.TestDbContextOptions()))
+            {
+                // Arrange
+                var expectedCustomers = DeliveryCartDbContext.GetSeedingCustomers();
+                await db.AddRangeAsync(expectedCustomers);
+                await db.SaveChangesAsync();
+
+                // Act
+                var result = await db.GetCustomersAsync();
+
+                // Assert
+                var actualCustomers = Assert.IsAssignableFrom<List<Customer>>(result);
+                Assert.Equal(
+                    expectedCustomers.OrderBy(m => m.CustomerID).Select(m => m.Email), 
+                    actualCustomers.OrderBy(m => m.CustomerID).Select(m => m.Email));
+            }
+        }
+    }
+}
